8fbc57537eb19c8b26a1a7513e65d9d393bf6ce4
Devina0810 <devinayadav8@gmail.com>
2025-07-05 16:29:51 +0530

    Update: fixed base URL for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
-    : "https://linkora-09tw.onrender.com/api";
-
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log("🌍 Base URL being used:", BASE_URL);
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
