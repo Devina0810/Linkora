74321525e911289057fed842c15e260177e861c5
Devina0810 <devinayadav8@gmail.com>
2025-07-05 17:58:28 +0530

    Frontend and backend commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-console.log("🌍 Base URL being used:", BASE_URL);
+const BASE_URL = import.meta.env.MODE_ENV === "development" ? "http://localhost:5001/api" : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
